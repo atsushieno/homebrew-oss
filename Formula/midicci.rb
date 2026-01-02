21cbe237f6833f6261ed8ccedde4e096b110ae0c
Atsushi Eno <atsushieno@gmail.com>
2026-01-02 13:17:41 +0900

    Add midicci.

---
+++ Formula/midicci.rb
@@ -0,0 +1,30 @@
+class Uapmd < Formula
+  desc "midicci MIDI-CI tools and libraries"
+  homepage "https://github.com/atsushieno/midicci"
+  url "https://github.com/atsushieno/midicci.git",
+      revision: "v0.1"
+  version "0.1.0"
+  license "MIT"
+  head "https://github.com/atsushieno/midicci.git", branch: "main"
+
+  depends_on "cmake" => :build
+  depends_on "ninja" => :build
+  depends_on "pkg-config" => :build
+  depends_on "qt@5"
+  depends_on "sdl3"
+
+  def install
+    ENV.append_path "CMAKE_PREFIX_PATH", Formula["qt@5"].opt_prefix
+
+    system "cmake", "-S", ".", "-B", "build", "-G", "Ninja",
+                    "-DCMAKE_BUILD_TYPE=Release",
+                    "-DCMAKE_INSTALL_PREFIX=#{prefix}",
+                    "-DUAPMD_VERSION=#{version}"
+    system "cmake", "--build", "build"
+    system "cmake", "--install", "build"
+  end
+
+  test do
+    system "#{bin}/remidy-scan", "--help"
+  end
+end
