10d91a0d132c1cc013d739ebf01742edb47b922c
Atsushi Eno <atsushieno@gmail.com>
2026-02-03 16:56:40 +0900

    uapmd 0.1.4 and midicci 0.1.4.

--- Formula/midicci.rb
+++ Formula/midicci.rb
@@ -2,8 +2,8 @@ class Midicci < Formula
   desc "midicci MIDI-CI tools and libraries"
   homepage "https://github.com/atsushieno/midicci"
   url "https://github.com/atsushieno/midicci.git",
-      revision: "0.1.3"
-  version "0.1.3"
+      revision: "0.1.4"
+  version "0.1.4"
   license "MIT"
   head "https://github.com/atsushieno/midicci.git", branch: "main"
 
